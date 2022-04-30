760cfe99baa1c0d9e78bd287c0e1f38a7ef31ec1
Alex <alex@alex-dev.me>
2022-04-30 18:14:18 -0500

    maruOS config

--- config.sh
+++ config.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="nalexcastaneda"
+export GITHUB_EMAIL="alex@alex-dev.me"
 
-export device=""
+export device="cedric"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export ROM="maruOS"
+export ROM_DIR="${WORKSPACE}/../maruOS/"
+export ROM_VERSION="0.7"
+export local_manifest_url="https://raw.githubusercontent.com/nalexcastaneda/local_manifests/main/cedric/maru/roomservice.xml"
+export manifest_url="https://github.com/maruos/manifest.git"
+export rom_vendor_name="maru"
+export branch="maru-0.7"
 export bacon="bacon"
 export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+ export clean="installclean"
+export generate_incremental="false"
+export upload_recovery="false"
 
-export ccache=""
-export ccache_size=""
+export ccache="true"
+export ccache_size="100"
 
-export jenkins="false"
+export jenkins="true"
 
-export release_repo=""
-
+export release_repo="nalexcastaneda/releases"
 export timezone="UTC"
